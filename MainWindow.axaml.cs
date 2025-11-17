e2f8e2cda599f59ce53d05bbe6651597b4e7876c
Tim Browne <timb@itmastrs.nt.au>
2025-11-17 16:18:40 +1000

    251117-1618

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,5 @@
 using Avalonia.Controls;
-
+using Avalonia.Interactivity;
 namespace CS_VSC_AvaloniaTest;
 
 public partial class MainWindow : Window
@@ -7,5 +7,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void Button1Handler(object sender, RoutedEventArgs Event_Args)
+    {
+        NetworkClass.TestConnect("https://rmm.itmasters.net.au/index.php");
+        TB_WebStatus.Text = NetworkClass.ResponseStatucCode;
+        TB_WebMessage.Text = NetworkClass.ResponseMessage;
     }
 }
--- NetworkClass.cs
+++ NetworkClass.cs
@@ -1,25 +1,32 @@
 using System.Net.Http;
+using System.IO;
 using System.Threading.Tasks;
+using Avalonia.Media;
 using Tmds.DBus.Protocol;
 
 namespace CS_VSC_AvaloniaTest;
 
-internal static class Network
+internal static class NetworkClass
 {
     static readonly HttpClient Http_Client = new HttpClient();
+    internal static string ResponseStatucCode = "";
+    internal static string ResponseMessage = "";
 
-    static Network() { }
+    static NetworkClass() { }
 
     internal static (bool, string) TestConnect(string URL)
     {
         string ReturnString = "";
         HttpRequestMessage Http_Request = new HttpRequestMessage();
-        Http_Request.RequestUri = new System.Uri("rmm.itmasters.net.au");
+        Http_Request.RequestUri = new System.Uri("http://rmm.itmasters.net.au/index.php"); 
         Http_Request.Method = HttpMethod.Get;
+        //Http_Request.Content = new StringContent("{ 'some': 'value' }", Encoding.UTF8, "application/json")
 
         HttpResponseMessage Http_Response = Http_Client.Send(Http_Request);
+        ResponseStatucCode = Http_Response.StatusCode.ToString();
+        StreamReader ResponseStream = new StreamReader(Http_Response.Content.ReadAsStream());
+        ResponseMessage = ResponseStream.ReadToEnd();
         
-
         return (true, ReturnString);
     }
     
